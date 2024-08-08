695cf7900b3fe68a3383a06701c3e2a8b8475f5c
phaniraj35 <phaniitng@gmail.com>
2024-08-08 18:04:51 +0530

    Hero section implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { Header } from "@/sections";
+import { Header, Hero } from "@/sections";
 
 export default function Home() {
   return <div className="w-full">
     <Header />
+    <Hero />
   </div>;
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
 import LogoIcon from './logo.svg';
 import MenuIcon from './icon-menu.svg';
+import starsBgImg from './stars.png'
 
 export {
     LogoIcon,
-    MenuIcon
+    MenuIcon,
+    starsBgImg
 }
