f310b9004f59ee31d6a0584843f716ccfed6007a
phaniraj35 <phaniitng@gmail.com>
2024-08-16 17:22:14 +0530

    Testimonials section added

--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Hero from "./Hero";
 import LogoTicker from "./LogoTicker";
 import Features from "./Features";
+import Testimonials from "./Testimonials";
 
-export { Header, Hero, LogoTicker, Features };
+export { Header, Hero, LogoTicker, Features, Testimonials };
